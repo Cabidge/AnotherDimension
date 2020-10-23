2f919c2b639c78af7e56921d670def8a898718fa
wdong20 <wdong20@stuy.edu>
2020-10-23 12:05:48 -0400

    Created sum method in ArrayOps, tested in Tester

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,5 +1,15 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static int sum(int[] arr) {
+        int out = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            out += arr[i];
+        }
+
+        return out;
     }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
-        
+        //int[] a = {1, 4, 3};
+        //System.out.println(ArrayOps.sum(a)); // Expected 8
     }
 }
