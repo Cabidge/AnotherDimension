11f5c68e0d9b9203967a8a3e2c8a17e3dc626632
wdong20 <wdong20@stuy.edu>
2020-10-24 11:25:16 -0400

    Removed main from ArrayOps and uncommented everything in tester

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,8 +1,4 @@
 public class ArrayOps {
-    public static void main(String[] args) {
-
-    }
-
     public static int sum(int[] arr) {
         int out = 0;
 
--- Tester.java
+++ Tester.java
@@ -1,17 +1,17 @@
 public class Tester {
     public static void main(String[] args) {
-        //int[] a = {1, 4, 3};
-        //System.out.println(ArrayOps.sum(a)); // Expected 8
-        //System.out.println(ArrayOps.largest(a)); // Expected 4
+        int[] a = {1, 4, 3};
+        System.out.println(ArrayOps.sum(a)); // Expected 8
+        System.out.println(ArrayOps.largest(a)); // Expected 4
 
-        //int[][] b = {
-        //    {1, 3, 8},
-        //    {4, 4, 4, 4},
-        //    {6, 2},
-        //};
-        //int[] x = ArrayOps.sumRows(b);
-        //for (int i = 0; i < x.length; i++) {
-        //    System.out.println(x[i]);
-        //}
+        int[][] b = {
+           {1, 3, 8},
+           {4, 4, 4, 4},
+           {6, 2},
+        };
+        int[] x = ArrayOps.sumRows(b);
+        for (int i = 0; i < x.length; i++) {
+           System.out.println(x[i]);
+        }
     }
 }
