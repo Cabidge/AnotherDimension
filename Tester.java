fd6e2deafe88c3a55092e40e7c3b776d86df20b7
wdong20 <wdong20@stuy.edu>
2020-10-24 11:29:08 -0400

    Added largestInRows into Arrayops, tested in Tester and made Terster's output more readable

--- Tester.java
+++ Tester.java
@@ -1,17 +1,26 @@
 public class Tester {
     public static void main(String[] args) {
         int[] a = {1, 4, 3};
+        System.out.println("Sum of 1, 4, 3:");
         System.out.println(ArrayOps.sum(a)); // Expected 8
+        System.out.println("\nLargest of 1, 4, 3:");
         System.out.println(ArrayOps.largest(a)); // Expected 4
 
+        System.out.println("\nMatrix:");
         int[][] b = {
            {1, 3, 8},
            {4, 4, 4, 4},
            {6, 2},
         };
+        System.out.println("Sum rows:");
         int[] x = ArrayOps.sumRows(b);
         for (int i = 0; i < x.length; i++) {
            System.out.println(x[i]);
+        }
+        System.out.println("\nLargest rows:");
+        int[] y = ArrayOps.largestInRows(b);
+        for (int i = 0; i < x.length; i++) {
+           System.out.println(y[i]);
         }
     }
 }
